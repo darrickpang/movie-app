be8b9a3c1cbba0a8f9cc3c073e7ccb970d9349a3
jlorenc1 <joshua.lorenc@icloud.com>
2020-07-30 17:39:18 -0700

    still working on crud stuff

--- movie-frontend/index.js
+++ movie-frontend/index.js
@@ -108,11 +108,12 @@ const showMovie = (e, movie) => {
     <h2>${movie.attributes.director}</h2>
     <h3>${movie.attributes.year}</h3>
     <p>${movie.attributes.description}</p>
-    <button id='like'>${EMPTY_HEART}</button> 
+    <button id='like'>♡</button> 
     <form id='comment-form'>
             <input id='comment' placeholder='comment'>
             <input type='submit' value='Leave Comment'>
     </form>
+    <button id='update'>Update Comment</button>
     <div id='comment-section'><ul id='movie-comments'></ul>
     </div>
     `
@@ -120,15 +121,29 @@ const showMovie = (e, movie) => {
     let ul = document.getElementById('movie-comments')
     movie.attributes.comments.forEach(comment => {
         let li = document.createElement('li')
+        li.id = 'liElement'
+        li.innerHTML = `<button id="edit" data-id=${comment.id}>Edit</button>
+                        <button id="delete" data-id=${comment.id}>Delete</button>`
         li.innerText = comment.content
         ul.appendChild(li)
 
         let editButton = document.createElement('button')
         editButton.innerText = 'Edit'
+        editButton.addEventListener('click', (e) => {
+            editComment(e, comment)
+        })
         li.append(editButton)
+
+        let updateComments = document.getElementById('update')
+        updateComments.addEventListener('click', (e) => {
+            updateComment(e, movie, comment)
+        })
 
         let deleteButton = document.createElement('button')
         deleteButton.innerText = 'Delete'
+        deleteButton.addEventListener('click', (e) => {
+            deleteComment(comment)
+        })
         li.append(deleteButton)
     })
     
@@ -144,6 +159,64 @@ const showMovie = (e, movie) => {
     commentForm.addEventListener('submit', (e) => {
         addComment(e, movie)
     })
+}
+
+
+const editComment = (e, comment) => {
+    fetch(`http://localhost:3000/comments/${comment.id}`)
+        .then(function(response){
+            return response.json()
+        })
+        .then(comment => {
+            let form = document.getElementById('comment-form')
+            console.log(form[0].value)
+            commentForm = form[0]
+            commentForm.value = comment.content
+            commentForm.dataset.id = comment.id
+        })
+    
+}
+
+const updateComment = (e, movie, comment) =>{
+    e.preventDefault()
+
+    
+    console.log(document.getElementById('liElement'))
+    let ul = document.getElementById('movie-comments')
+    let editComment = document.getElementById('liElement')
+    
+    let form = document.getElementById('comment-form')
+    console.log(form[0].value)
+    editComment.textContent = form[0].value
+    // ul.appendChild(editComment)
+    
+    // let edit = ul.querySelector(li)
+    // let editButton = document.createElement('button')
+    // edit.append(editButton)
+
+    let data = {content: form[0].value, movie_id: movie.id, user_id: found_user_id}
+    
+    fetch(`http://localhost:3000/comments/${comment.id}`, {
+        method: 'PATCH', 
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify(data),
+    }) 
+}
+
+const deleteComment = (comment) =>{
+    console.log(comment.id)
+    let clear = event.target.parentElement
+    clear.remove()    
+    console.log(event.target.parentElement)
+    fetch(`http://localhost:3000/comments/${comment.id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }) 
 }
 
 const likeStatus = (e, movie) =>{                       // NEEDS TO BE FIXED!!!
